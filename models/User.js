0e54f666ef0a7ba0ad1ae448497d6d53723271fb
chrismillsnbcu <chris.mills@nbcuni.com>
2014-08-06 11:16:41 -0700

    user model

--- models/User.js
+++ models/User.js
@@ -9,22 +9,40 @@ var keystone = require('keystone'),
 var User = new keystone.List('User');
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true }
+  name: { type: Types.Name, required: true, index: true },
+  email: { type: Types.Email, initial: true, required: true, index: true },
+  company: { type: String, required: true, index: true, initial: true },
+  phone: { type: String, required: true, index: true, initial: true },
+  password: { type: Types.Password, initial: true, required: true },
+  createdBy: { type: Types.Admin, required: true, initial: true },
+  groupId: { type: Types.Admin, required: true, initial: true },
+  createdAt: { type: Types.Date, default: Date.now }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
-});
+  level : { type: Types.Select, numeric: true, options: [{ value: 1, label: 'User' }, { value: 2, label: 'Group Administrator' }, { value: 3, label: 'System Administrator' }] }
+},
+  'Screening', {
+    //isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+    rooms : { type: Types.Select, numeric: true, options: [{ value: 1, label: 'Screening room 1' }, { value: 2, label: 'Screening room 2' }, { value: 3, label: 'Screening room 3' }] }
+  });
 
 // Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function() {
-	return this.isAdmin;
+	// Return true if admin
+  if(this.level === 2 || this.level === 3) {
+    return true;
+  } else {
+    return false;
+  }
 });
 
+// Provide screening room access
+User.schema.virtual('screening').get(function() {
+  return this.rooms;
+});
 
 /**
  * Registration
  */
 
-User.defaultColumns = 'name, email, isAdmin';
+//User.defaultColumns = 'name, email, company, phone, level, rooms';
 User.register();
