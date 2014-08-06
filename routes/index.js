d51f87da1f78427d0d3176438baf837c615d5d12
chrismillsnbcu <chris.mills@nbcuni.com>
2014-08-06 09:48:51 -0700

    init

--- routes/index.js
+++ routes/index.js
@@ -37,8 +37,6 @@ exports = module.exports = function(app) {
 	
 	// Views
 	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
 	
 	
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
