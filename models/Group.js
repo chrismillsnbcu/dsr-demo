130367269d99999b6b237bcbb4499d6e57a6f612
chrismillsnbcu <chris.mills@nbcuni.com>
2014-08-05 19:53:19 -0700

    test

--- models/Group.js
+++ models/Group.js
@@ -11,7 +11,7 @@ var Group = new keystone.List('Group');
 Group.add({
   name: { type: String, required: true, initial: true },
 	createdAt: { type: Date, default: Date.now },
-  groupId: { type: Types.Admin, required: true, initial: true, default: 'placeholder' }
+  //groupId: { type: Types.Admin, required: true, initial: true, default: 'placeholder' }
 });
 
 Group.defaultColumns = 'name';
